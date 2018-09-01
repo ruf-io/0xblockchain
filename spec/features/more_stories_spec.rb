d6925cc0d81fc4edb8f072d93a50d76fa5fe84f0
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2018-09-01 23:33:31 +0700

    Add more stories feature

---
+++ spec/features/more_stories_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "More stories feature" do
+  scenario "Stories count less than equal 25" do
+    # Create dummy story using factory bot
+    create_list :story, 25
+
+    # Visit homepage
+    visit root_path
+
+    # Story should be available on the front-page
+    expect(page).to_not have_content "More Stories"
+  end
+
+  scenario "Stories count larger than 25" do
+    # Create dummy story using factory bot
+    create_list :story, 30
+
+    # Visit homepage
+    visit root_path
+    # Link "More Stories" should be accessible
+    expect(page).to have_content "More Stories"
+    expect(page).to have_selector(:xpath,
+                                  "/html/body/div/div[3]/ol/li[25]")
+
+    # Click more stories
+    click_link "More Stories"
+    # Then test the next page
+    expect(page).to_not have_content "More Stories"
+    expect(page).to have_selector(:xpath,
+                                  "/html/body/div/div[3]/ol/li[5]")
+  end
+end
