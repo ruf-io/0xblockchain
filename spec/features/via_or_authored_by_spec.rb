4c207b5b6e4aab88fe21672853882d3d0944753b
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2018-09-02 00:10:18 +0700

    Add via or authored by feature spec

---
+++ spec/features/via_or_authored_by_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "Via or Authored by" do
+  scenario "via" do
+    # Create dummy story using factory bot
+    story = create :story, user_is_author: false
+
+    # Visit homepage
+    visit root_path
+    expect(page).to have_content "via " + story.user.username
+
+    # Make sure link to the user profile is work
+    click_link story.user.username
+    expect(page).to have_current_path "/u/" + story.user.username
+  end
+
+  scenario "authored by" do
+    # Create dummy story using factory bot
+    story = create :story, user_is_author: true
+
+    # Visit homepage
+    visit root_path
+    expect(page).to have_content "authored by " + story.user.username
+
+    # Make sure link to the user profile is work
+    click_link story.user.username
+    expect(page).to have_current_path "/u/" + story.user.username
+  end
+end
